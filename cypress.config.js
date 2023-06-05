be1873b2f59e067acd8bac6a82664882f886403e
Bill Glesias <bglesias@gmail.com>
2023-06-05 16:40:43 -0400

    chore: attempt to persist recording artifact and check for bugs

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId: "mo4rcs"
+    projectId: "mo4rcs",
+    video: true
   },
 });
